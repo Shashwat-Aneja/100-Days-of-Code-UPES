578b8f638dc8a6cf60419c44fc51772b4b27490b
Shashwat Aneja <shashwataneja1401@gmail.com>
2026-09-14 10:25:26 +0530

    Add Day 23 Q46 5x5 star pattern

---
+++ Day-23/Q46.c
@@ -0,0 +1,15 @@
+// Q46: Write a program to print a 5x5 star pattern.
+#include <stdio.h>
+
+int main(void) {
+    int i, j;
+
+    for (i = 1; i <= 5; i++) {
+        for (j = 1; j <= 5; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
