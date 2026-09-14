0e8a6e05a2c0ce0b299b35aae212c35a13a14435
Shashwat Aneja <shashwataneja1401@gmail.com>
2026-09-14 10:25:30 +0530

    Add Day 24 Q47 increasing star triangle

---
+++ Day-24/Q47.c
@@ -0,0 +1,11 @@
+// Q47: Write a program to print an increasing star triangle.
+#include <stdio.h>
+
+int main(void) {
+    int i, j;
+    for (i = 1; i <= 5; i++) {
+        for (j = 1; j <= i; j++) printf("* ");
+        printf("\n");
+    }
+    return 0;
+}
