3037bd20c840a5012b1626b382b5942d12f35ab0
Shashwat Aneja <shashwataneja1401@gmail.com>
2026-09-14 10:26:00 +0530

    Add Day 27 Q54 centered diamond

---
+++ Day-27/Q54.c
@@ -0,0 +1,17 @@
+// Q54: Print a centered diamond star pattern.
+#include <stdio.h>
+
+int main(void) {
+    int i, j;
+    for (i = 1; i <= 4; i++) {
+        for (j = 1; j <= 4 - i; j++) printf(" ");
+        for (j = 1; j <= 2 * i - 1; j++) printf("*");
+        printf("\n");
+    }
+    for (i = 3; i >= 1; i--) {
+        for (j = 1; j <= 4 - i; j++) printf(" ");
+        for (j = 1; j <= 2 * i - 1; j++) printf("*");
+        printf("\n");
+    }
+    return 0;
+}
