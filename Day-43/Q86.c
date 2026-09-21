68c4de5097b795151a0acc98c0148dc9610c9d3f
Shashwat Aneja <shashwataneja1401@gmail.com>
2026-09-21 08:52:02 +0530

    Add Day 43 Q86: Check string palindrome

---
+++ Day-43/Q86.c
@@ -0,0 +1,31 @@
+// Q86. To check if a string is a palindrome.
+
+#include <stdio.h>
+
+int main(void) {
+    char str[1000];
+    int length = 0;
+    int isPalindrome = 1;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[length] != '\0' && str[length] != '\n') {
+        length++;
+    }
+
+    for (int i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - 1 - i]) {
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    if (isPalindrome) {
+        printf("The string is a palindrome.\n");
+    } else {
+        printf("The string is not a palindrome.\n");
+    }
+
+    return 0;
+}
